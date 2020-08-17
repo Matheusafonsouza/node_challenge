0cca97d421e1b22e56a0e0efb1b8ee7fddffa9e9
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-17 18:57:07 -0300

    feature: Created update method for repositories

--- src/app.js
+++ src/app.js
@@ -32,7 +32,27 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.json(400).json({ error: 'Repository not found.' });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.status(200).json(repository);
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
